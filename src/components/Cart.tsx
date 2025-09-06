3d4ef4d635a716b04632e659630e1cb070e6ec7d
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-09-07 02:14:41 +0530

    Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,3 +1,75 @@
+"use client";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { formatPrice } from "@/lib/utils";
+import { Separator } from "@radix-ui/react-separator";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 export default function Cart() {
-  return <div>Cart</div>;
+  const itemCount = 1;
+  const fee = 1;
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart
+          aria-hidden="true"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+        >
+          <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+            0
+          </span>
+        </ShoppingCart>
+      </SheetTrigger>
+      {/* The content of the cart will be here */}
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* Todo Cart Logic */}
+              Cart Items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({ className: "w-full" })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className=""></div>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
 }
